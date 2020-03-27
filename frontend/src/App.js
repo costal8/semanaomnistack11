e857a7aa3cafb5340b03f8f1d47960f804386bd3
costal8 <luisinstrumentacao@hotmail.com>
2020-03-26 23:53:21 -0300

    front-end meio

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Header from './Header';
+import Logon from './pages/Logon';
+import './global.css';
 
 function App() {
+  const [counter, setCounter ]= useState(0);
+  // Array[valor, funcao de atualização]
+  
+  function increment() {
+    setCounter(counter + 1);
+    
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          <p>LUIS Alterou a tela</p>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+       <Header title="Semana OminiStack">
+         
+         Semana OminiStack2
+         Semana
+
+       </Header> 
+       
+        <p> Contador:{counter}</p>
+         <button onClick={increment}>Incrementar</button>
+
+        <Logon />
     </div>
+
+        
   );
 }
 
