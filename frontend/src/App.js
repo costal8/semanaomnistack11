2e41d306ce1f21ad7f4108cb336c6a6bf6932533
costal8 <luisinstrumentacao@hotmail.com>
2020-03-27 22:01:30 -0300

    front-end meio

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,11 @@
 import React, { useState } from 'react';
-import Header from './Header';
-import Logon from './pages/Logon';
 import './global.css';
 
+import Routers from './routes';
+
 function App() {
-  const [counter, setCounter ]= useState(0);
-  // Array[valor, funcao de atualização]
-  
-  function increment() {
-    setCounter(counter + 1);
-    
-  }
-  return (
-    <div>
-       <Header title="Semana OminiStack">
-         
-         Semana OminiStack2
-         Semana
-
-       </Header> 
-       
-        <p> Contador:{counter}</p>
-         <button onClick={increment}>Incrementar</button>
-
-        <Logon />
-    </div>
-
-        
+   return (    
+      <Routers />        
   );
 }
 
